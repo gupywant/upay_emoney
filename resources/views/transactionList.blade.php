d6376266390852d8186bd11ffeca0b0836caff8c
Gupy Wantoro <gupywant21@gmail.com>
2021-03-15 10:43:24 +0700

    update transaction report

--- resources/views/transactionList.blade.php
+++ resources/views/transactionList.blade.php
@@ -66,7 +66,10 @@
 	                      {{$data->note}}
 	                    </td>
 	                    <td>
-	                      {{$data->amount + ($data->amount * 0.02)}}
+	                    	@php
+	                    		$amount = Session::get('id') !== $data->id_user ? $data->amount + ($data->amount * 0.02) : $data->amount;
+	                    	@endphp
+	                      {{$amount}}
 	                    </td>
 	                    <td>
 	                      {{$data->created_at}}
